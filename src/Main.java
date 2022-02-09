0deb845743e9056348606a07bb3c7728ee3b2bd3
ALEKSEY <hattaeck@gmail.com>
2022-02-09 09:56:26 +0300

    add .gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-        int currentaccbalance = 2_000_000_000;
-        int transfersumm = 500_000_000;
-        int totalsumm = transfersumm + currentaccbalance;
-        System.out.println(totalsumm);
+        int currentBalance= 2_000_000_000;
+        int transferSumm = 500_000_000;
+        int totalSumm = transferSumm + currentBalance;
+        System.out.println(totalSumm);
 
     }
 }
